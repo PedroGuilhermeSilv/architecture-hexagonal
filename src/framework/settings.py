33adc0aa317ef95090b4c97843a318f176656b4b
guilherme <vinespedro55@gmail.com>
2024-09-05 12:11:27 -0300

    fix: static files

--- src/framework/settings.py
+++ src/framework/settings.py
@@ -149,3 +149,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
